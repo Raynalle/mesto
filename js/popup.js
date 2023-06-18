59ea10de4ab50573eeeb7d8158d502d95db9f040
Dmitriy Lebedev <dante1213@yandex.ru>
2023-06-18 15:03:19 +0300

    Сделаны правки

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,20 @@
+const editButtonElement = document.querySelector('.profile__button-edit');
+const closeButtonElement = document.querySelector('.popup__button-close');
+const popupElement = document.querySelector('.popup');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#description');
+const nameProfile = document.querySelector('.profile__title');
+const jobProfile = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector(".popup__form-profile");
+
 function handleClick(e) {
     e.preventDefault();
     e.stopPropagation();
 
     editButtonElement.blur();
+    
+    nameProfile.value = nameInput.textContent;
+    jobProfile.value = jobInput.textContent;
 
     popupElement.classList.toggle('popup_open');
     
@@ -14,28 +26,13 @@ function handleFormSubmit(evt) {
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
 
-    popupElement.classList.toggle('popup_open');
+    popupElement.classList.remove('popup_open');
 }
 
-const editButtonElement = document.querySelector('.profile__button-edit');
-const closeButtonElement = document.querySelector('.popup__button-close');
-const popupElement = document.querySelector('.popup');
+
 
 
 editButtonElement.addEventListener('click', handleClick);
 closeButtonElement.addEventListener('click', handleClick);
-
-const nameInput = document.querySelector('#name');
-const jobInput = document.querySelector('#description');
-const nameProfile = document.querySelector('.profile__title');
-const jobProfile = document.querySelector('.profile__subtitle');
-const formElement = document.querySelector(".popup__form-profile");
-
-let likeElement = document.querySelector('.element__like');
-function likeActive() {
-    likeElement.classList.toggle('element__like_active');
-}
-
-likeElement.addEventListener('click', likeActive);
 
 formElement.addEventListener("submit", handleFormSubmit);
